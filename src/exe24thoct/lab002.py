7510396e8c5c18312529f5d92d5f464f888c80af
Abhi-Dash <abinash1205@gmail.com>
2024-11-11 07:58:34 +0530

    Delete src/exe24thoct directory

--- src/exe24thoct/lab002.py
+++
@@ -1,7 +0,0 @@
-# Write a program to check whether the number is an odd nuber or Even number
-
-a = int (input("Enter a number to check if its Odd or Even :"))
-if a%2 ==0:
-    print ("The Entered number {} is an Even Number".format(a))
-else:
-    print("The Entered number {} is an ODD Number".format(a))
--- src/exe24thoct/scratch.py
+++
@@ -1,7 +0,0 @@
-#Leap Year Checker:
-
-year = int (input("Enter any year to check : "))
-if (year%4==0 and year%100 !=0) or (year%400==0):
-    print("{} Year is a leap year".format(year))
-else:
-    print ("{} year is not a leap year".format(year))
